6b9a7107bcec4641a22105e7140d11e720fb53f5
Caio <bladellano@gmail.com>
2022-05-08 08:24:36 -0300

    Fetching Data

--- src/views/jobs/JobDetailsView.vue
+++ src/views/jobs/JobDetailsView.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    <h1>Job Details</h1>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
     The job id is {{ id }}
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading job details...</p>
   </div>
 </template>
 
 <script>
 export default {
-    props:['id'],
- /*    data(){
+  props: ["id"],
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch(`http://localhost:3000/jobs/${this.id}`)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+  /*    data(){
         return {
             id:this.$route.params.id
         }
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-      <router-link :to="{ name: 'job-details', params: { id: job.id } }">
-        <h2>{{ job.title }}</h2>
-      </router-link>
+    <div v-if="jobs.length">
+      <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'job-details', params: { id: job.id } }">
+          <h2>{{ job.title }}</h2>
+        </router-link>
+      </div>
+    </div>
+    <div v-else>
+      <p>Loading jobs...</p>
     </div>
   </div>
 </template>
@@ -13,12 +18,14 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "Lorem abc", id: 1, details: "Lorem ipsum is simply" },
-        { title: "Lorem def", id: 2, details: "Lorem ipsum is simply" },
-        { title: "Lorem ghi", id: 3, details: "Lorem ipsum is simply" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
